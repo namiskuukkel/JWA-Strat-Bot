f503391db91ea3861db77c101fd81ce1ed7f43c2
namiskuukkel <namiskuukkel@gmail.com>
2021-01-29 14:38:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,5 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 console.log('I am!');
+console.log(process.env.THINGY);
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
