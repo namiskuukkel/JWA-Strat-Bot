b06cfc8b41055c3904eccd5687e3106347380dcb
R H <namiskuukkkel@gmail.com>
2021-01-29 19:50:07 +0200

    -Testing what information is available of message attachments

--- bot.js
+++ bot.js
@@ -8,12 +8,11 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    console.log(message.author.id);
-    console.log(message.author.id === process.env.BOT_ID);
     if (!message.author || message.author.id === process.env.BOT_ID  || message.author.bot)
         return;
     const regEx = new RegExp(`^${prefix}(?:set|s)`);
     if (regEx.exec(message.content)) {
+        console.log(message.attachments);
         const attachment = 'https://i.imgur.com/w3duR07.png';
         // Send the attachment in the message channel
         message.channel.send("Here!", {
