fb296398dc81720eb57b3fa2ec241d60083a359a
R H <namiskuukkkel@gmail.com>
2021-01-29 18:28:34 +0200

    Initial matching for commands

--- bot.js
+++ bot.js
@@ -1,29 +1,23 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
- 
+const prefix = '!sb';
 
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
 
- 
-
 client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
+    const regEx = new RegExp(`^${prefix}(?:set|s)`);
+    if (regEx.exec(message.content)) {
+       message.reply("It's a match!");
+    } else {
+        message.reply("Que?");
+    }
 
 });
 
  
 
 // THIS  MUST  BE  THIS  WAY
-console.log('I am!');
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
