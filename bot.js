88f016ceb8be06278676d9ed44a9fb45191d8f79
R H <namiskuukkkel@gmail.com>
2021-01-29 18:56:12 +0200

    Stop answering to everything

--- bot.js
+++ bot.js
@@ -14,10 +14,7 @@ client.on('message', message => {
     const regEx = new RegExp(`^${prefix}(?:set|s)`);
     if (regEx.exec(message.content)) {
        message.reply("It's a match!");
-    } else {
-        message.reply("Que?");
     }
-
 });
 
 client.login(process.env.BOT_TOKEN);
